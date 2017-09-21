16a6244ab84b5fb58d39c22a5e1bf09a522ac0a5
picheljitsu <pichelmayer@gmail.com>
2017-09-21 03:15:03 -0500

    Parses NMAP output by stripping out Alive hosts

---
+++ get-alives.ps1
@@ -0,0 +1,19 @@
+﻿#Take in an NMAP -sn -oG file and strip out terms
+function get-alives{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True,Position=0)]
+            [String]$infile,
+        [Parameter(Mandatory=$False,Position=1)]
+            [String]$outfile
+        )
+
+    $r = [regex] "\(([l].*?)(\.<#DOMAINNAMEHERE#>\))"
+    $file_contents = Get-Content $infile
+    foreach($line in $file_contents){
+        $match = $r.match($line)
+        if($match.groups[1].value){
+            $match.groups[1].value | Tee-Object -FilePath $outfile -append | foreach {write-host $_}
+            }
+        }
+    }
