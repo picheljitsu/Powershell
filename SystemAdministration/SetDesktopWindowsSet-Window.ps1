b5aab4ce8341790b7772c0c3ba799a3568f3516c
Matt Pichelmayer <pichelmayer@gmail.com>
2018-07-15 12:20:31 -0500

    Update SetDesktopWindowsSet-Window.ps1

--- SystemAdministration/SetDesktopWindowsSet-Window.ps1
+++ SystemAdministration/SetDesktopWindowsSet-Window.ps1
@@ -1,8 +1,5 @@
-    <#Not done. Need to work on calling logic and wrap in function
 
-    Author: Matt Pichelmayer 
-    Going to add functionality to compile this as a service exe so you can set windows up automatically 
-    #>
+
     [CmdletBinding()]
     param([parameter]
           [int32]$Process,
@@ -19,16 +16,19 @@
     [string]$user32_assembly_import += '[DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);'
     $script:user32_dll = Add-Type -MemberDefinition $user32_assembly_import -name NativeMethods -namespace Win32
 
-    function Maximize($Process){
+    function Set-Mize($Process, [Switch]$Maximize, [switch]$Minimize){
+
+        [string]$user32_assembly_import =  '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+        [string]$user32_assembly_import += '[DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);'
+        $user32_dll = Add-Type -MemberDefinition $user32_assembly_import -name NativeMethods -namespace Win32
+        
+        if($Maximize){ $option = 3 }
+        if($Minimize){ $option = 4 }
         $hwnd = $process.MainWindowHandle
-        $user32_dll::ShowWindowAsync($hwnd, 3)
-        $user32_dll::SetForegroundWindow($hwnd) 
-    }
-    function Minimize($Process){
-        $hwnd = $process.MainWindowHandle
-        $user32_dll::ShowWindowAsync($hwnd, 4)
-        $user32_dll::SetForegroundWindow($hwnd) 
-    }
+        [Win32.NativeMethods]::ShowWindowAsync($hwnd, $option)
+        [Win32.NativeMethods]::SetForegroundWindow($hwnd) 
+
+        }
 
     Function Set-WindowPosition {
 
@@ -36,14 +36,20 @@
         [cmdletbinding()]
         Param (
             [parameter(ValueFromPipelineByPropertyName=$True)]
-            $ProcessName,
+            [object]$Process,
+            [parameter(ValueFromPipelineByPropertyName=$True)]
             [int]$X,
+            [parameter(ValueFromPipelineByPropertyName=$True)]
             [int]$Y,
+            [parameter(ValueFromPipelineByPropertyName=$True)]
             [int]$Width,
+            [parameter(ValueFromPipelineByPropertyName=$True)]
             [int]$Height,
-            [switch]$Passthru
-        )
+            [parameter(ValueFromPipelineByPropertyName=$True)]
+            [switch]$Passthru )
+
         Begin {
+
             Try{
                 [void][Window]
             } Catch {
@@ -69,8 +75,9 @@
             }
         }
         Process {
+            $Processname = $Process.name 
             $Rectangle = New-Object RECT
-            $Handle = [system.intptr](Get-Process -Name $Processname).MainWindowHandle
+            $Handle = [system.intptr]$Process.MainWindowHandle
             $Return = [Window]::GetWindowRect($Handle,[ref]$Rectangle)
             If (-NOT $PSBoundParameters.ContainsKey('Width')) {            
                 $Width = $Rectangle.Right - $Rectangle.Left            
@@ -139,31 +146,9 @@
         return $process 
 
         }
+    #Example run
+    #$proc = get-process -name *chrome*
+    #Set-Mize -Process $proc -Maximize }
+    #Set-WindowPosition -Process $proc -x 2625 -y 0 -Width 439 -Height 1087
 
 
-    # Need to use a program like Window Spy or programmatically detect window postions so you can set the coordinates where you 
-    # want them
-    # $steam_exe_path = "C:\Program Files (x86)\Steam\Steam.exe"
-    # $steam_arg = "friends"
-    # $teamspeak_exe_path = "C:\Program Files\TeamSpeak 3 Client\ts3client_win64.exe"
-
-    # $steam_proc = get-process -name "Steam"
-    # $tspeak_proc = get-process -name "ts3client_win64"
-
-    # if($steam_proc)
-    #         { Set-WindowPosition -ProcessName $steam_proc.name -x 3284 -y 12 }
-    # else    { Start-ProcessToForeground -FilePath $steam_exe_path -ArgumentList $steam_arg }
-
-    # #TeamSpeak
-    # if($tspeak_proc){ 
-
-    # $TeamSpeak_window = get-process steam | ? { $_.MainWindowTitle -eq "Friends" }
-    # Set-WindowPosition -InputObject $TeamSpeak_window -x 2908 -y 12 
-
-    # }
-
-    # else {  
-    
-    # Start-ProcessToForeground -FilePath $TeamSpeak_exe_path 
-    
-    #     }
