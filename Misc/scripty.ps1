56bcf2379407827caca53465fe23f2f24935e3da
Matt Pichelmayer <pichelmayer@gmail.com>
2018-08-12 22:42:30 -0500

    scripty.ps1

---
+++ Misc/scripty.ps1
@@ -0,0 +1,27 @@
+$signature=@' 
+      [DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+      public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@ 
+
+$mc = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru 
+
+$a = $True
+while(1){
+
+    $d = ([System.Windows.Forms.Cursor]::Position).y * .005
+
+    switch([System.Windows.Forms.UserControl]::MouseButtons) {
+    
+        "Left"  {  echo "Left click"
+                   $mc::mouse_event(0x01, 0, $d , 0, 0)
+                   sleep -Milliseconds .8
+                   #$mc::(0x00000004, 0, 0, 0, 0)
+                    }
+        "None"  {
+                  #$mc::mouse_event(0x00000010, 0, 0, 0, 0); 
+                    }
+
+        }
+
+    
+    }
