596bae53cccecabc2a58a750681c3e5f0db739f2
Matt Pichelmayer <pichelmayer@gmail.com>
2018-08-19 13:33:10 -0500

    Update scripty.ps1

--- Misc/scripty.ps1
+++ Misc/scripty.ps1
@@ -1,32 +1,68 @@
-#hit the windows key, type powershell
-#right-click powershell_ISE and select "Run-As administrator"
-#Run Set-ExecutionPolicy Unrestricted
-#past the below in and run
+function Invoke-Recoil {
+    [CmdletBinding()]
+    param([parameter]
+          [int]$Move,
+          [parameter]
+          [int]$Delay)
 
-$signature=@' 
-      [DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
-      public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
-'@ 
-
-$mc = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru 
-
-$a = $True
-while(1){
-
-    $d = ([System.Windows.Forms.Cursor]::Position).y * .005
-
-    switch([System.Windows.Forms.UserControl]::MouseButtons) {
+    $State = [hashtable]::Synchronized(@{ "Started" = $True })
+    $MouseThreadEnv = [RunspaceFactory]::CreateRunspace()
+    $MouseThreadEnv.Open()
+    $MouseThreadEnv.SessionStateProxy.SetVariable('State',$State)
+    $MouseThread = [PowerShell]::Create()
+    $MouseThread.runspace = $MouseThreadEnv
     
-        "Left"  {  echo "Left click"
-                   $mc::mouse_event(0x01, 0, $d , 0, 0)
-                   sleep -Milliseconds .8
-                   #$mc::(0x00000004, 0, 0, 0, 0)
+    #$Move = .007
+    #$Delay = .8
+    #$MouClickParams = @{ "Move"  = $Move
+    #                     "Delay" = $Delay }
+    #
+    $MouseClickBlock = {
+    
+                    
+                    $signature='[DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]'
+                    $signature += 'public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);'
+                        
+                    $mc = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru 
+                    $d = ([System.Windows.Forms.Cursor]::Position).y * .003
+                    
+                    while(1){
+                    
+                        switch([System.Windows.Forms.UserControl]::MouseButtons) {
+                        
+                            "Left"  {  $mc::mouse_event(0x01, 0, $d , 0, 0)
+                                       sleep -Milliseconds 2 }
+                    
+                            }
+                                      
+                        }
+    
                     }
-        "None"  {
-                  #$mc::mouse_event(0x00000010, 0, 0, 0, 0); 
-                    }
+    
+    #$msg = ''
+    #$menuOpts = @{ "AK" = @{ "Move" = ".008" ; "Delay" = $Delay = ".8" }
+    #               "AR" = @{ "Move" = ".007"; "Delay" = $Delay = ".8" } }
+    #$Choices = [array]$menuOpts.Keys
+    #write-host $Choices[0]
+    #$Choices += "Quit"
+    #
+    #$Optscount = $menuOpts.Count - 1
+    #write-host $Optscount
 
+    Clear-Host
+    $start = Read-host "Press enter to start or type QUit to quit, beeitch"
+    [void]$MouseThread.AddScript($MouseClickBlock)
+    $MouseThread.BeginInvoke() #| Out-Null
+    sleep 2
+
+    $runnin = $True
+    while($runnin){ 
+        $msg = ''
+        write-host $msg
+        $stop = read-host "Press any key to stop"
+        $MouseThread.stop()        
+        $start = read-host "Press any key to start" 
+        $MouseThread.start()
         }
 
-    
-    }
+    }              
