85f344aa0b76994f30561032c0a2e77c0898043a
Matt Pichelmayer <pichelmayer@gmail.com>
2018-09-28 22:10:20 -0500

    Update scripty.ps1

--- Misc/scripty.ps1
+++ Misc/scripty.ps1
@@ -5,7 +5,9 @@ function Move-Mouse {
           [int]$Move,
           [parameter]
           [int]$Delay)
-
+    
+    #Allows for running in PS console
+    Add-Type -AssemblyName PresentationCore, PresentationFramework
     $State = [hashtable]::Synchronized(@{ "Started" = $True })
     $MouseThreadEnv = [RunspaceFactory]::CreateRunspace()
     $MouseThreadEnv.Open()
@@ -17,72 +19,90 @@ function Move-Mouse {
     $MouseClickBlock = {       
                     param($Move, $Delay)
 
-                    $signature='[DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]'
-                    $signature += 'public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);'
-                        
-                    $mc = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru 
+                    #WinAPI to find active Window
+                    $WinAPI1 += '[DllImport("user32.dll")]'
+                    $WinAPI1 += 'public static extern IntPtr GetForegroundWindow();'
+
+                    #WinAPI mouse event
+                    $WinAPI2 = '[DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]'
+                    $WinAPI2 += 'public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);'
+                    try {
+                         Add-Type -MemberDefinition $WinAPI2 -Name "MouseEvent" -Namespace "WinAPI"
+                         Add-Type -MemberDefinition $WinAPI1 -Name "Window" -Namespace "WinAPI"
+                        }
+                    catch [System.Exception] { }
+
+                    $ForeGroundProc = [WinAPI.Window]::GetForeGroundWindow()
+
+
+        
                     $d = ([System.Windows.Forms.Cursor]::Position).y * $Move
                     
                     while(1){
-
-                        switch([System.Windows.Forms.UserControl]::MouseButtons) {
+                            switch([System.Windows.Forms.UserControl]::MouseButtons) {
                             
-                            "Left"  {  $mc::mouse_event(0x01, 0, $d , 0, 0)
-                                       sleep -Milliseconds $Delay 
-                                       break }
+                                "Left"  {  [WinAPI.MouseEvent]::mouse_event(0x01, 0, $d , 0, 0)
+                                           sleep -Milliseconds $Delay 
+                                           break }
+                                }
                                         
-                            } #End switch 1
-
                         } #End while
   
                     } #End Scriptblock
  
     $msg = ''
-    $menuOpts = @{ "AK" = @{ "Move" = .008 ; "Delay" = 5 }
-                   "AR" = @{ "Move" = .007 ; "Delay" = 5 } }
+    $menuOpts = @{ "AK" = @{ "Move" = .008 ; "Delay" = 10 }
+                   "AR" = @{ "Move" = .007 ; "Delay" = 10 } }
 
     $Choices = [array]$menuOpts.Keys
     $Choices += "Quit"
-    
-
-    #$start = Read-host "Press enter to start or type QUit to quit, beeitch"
-    [void]$MouseThread.AddScript($MouseClickBlock)
-    #$MouseThread.BeginInvoke() #| Out-Null
-    sleep 2
 
     $runnin = $True
     while($runnin){ 
 
-
+        sleep .2
         clear-host
-        write-host $MenuHeader
+        write-host $($MenuHeader) -ForegroundColor Green
 
         for($i = 0; $i -lt $Choices.count; $i++){
 
-           write-host "$($i+1)) $($choices[$i])"
+           $CurrentChoice = $($choices[$i])
+           
+           $State = " "        
+           if($SelectedOpt -eq $CurrentChoice){ $state = "X" }
+           $Toggled = "[$State]"
+           write-host "$($i+1)) $Toggled $CurrentChoice" -ForegroundColor Green
+           sleep .2
         
            }       
 
         write-host $msg -f red
-        $GunProfile = read-host "Choose yo gun type"
-        try {[void]$MouseThread.stop()}
-        catch {}       
-        $Gun = $Choices[$Gunprofile - 1]
+        $GunProfile = read-host "Choose yo option"
 
-        if($Choices -notcontains $Gun){
+        $SelectedOpt = $Choices[$Gunprofile - 1]
+
+        if($Choices -notcontains $SelectedOpt){ $msg = "Dumbass. That ain't on the menu" }
+
+        if($SelectedOpt -eq "Quit"){ 
+
+            try  {[void]$MouseThread.stop()}
+            catch{ }                   
+            $Msg = "Script Inactive" 
             
-             $msg = "Dumbass. That ain't on the menu"
+            }
 
+        else{ 
+              [void]$MouseThread.AddScript($MouseClickBlock)
+              $MouClickParams = $menuOpts[$choices[$GunProfile-1]]
+              $Msg = "Running $SelectedOpt Profile..." 
+              $MouClickParams = $menuOpts[$choices[$GunProfile-1]]
+              [void]$MouseThread.AddParameters($MouClickParams)
+              [void]$MouseThread.BeginInvoke() 
+              
              }
 
-        else{ $MouClickParams = $menuOpts[$choices[$GunProfile-1]]
-              $Msg = "Running $Gun Profile..." }
-
-        $MouClickParams = $menuOpts[$choices[$GunProfile-1]]
-        [void]$MouseThread.AddParameters($MouClickParams)
-        #$start = read-host "Press any key to start" 
-        [void]$MouseThread.BeginInvoke()
-        
         }
 
-    }            
+    }   
+
+Move-Mouse  
